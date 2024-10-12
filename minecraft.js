e94b9090b450e2aff11a78009e3a76bdc1011b9f
nana232002 <nanaomar457@gmail.com>
2024-10-12 14:59:50 +0300

    3

--- minecraft.js
+++ minecraft.js
@@ -1,17 +1,60 @@
 
 const soilHtml =document.querySelector(".container");
+const equb =document.querySelector(".equb");
+let soilCounter = 0;
+let soilbutton = document.createElement("button");
+soilHtml.appendChild(soilbutton);
+soilbutton.textContent = `grass = ${soilCounterCounter}`;
 
-for(let i=5 ;i<9 ;i++){
-    for(let x=1 ;x<11 ;x++){
-const imgSoil = document.createElement("img");
-imgSoil.src="soil1.png";
-imgSoil.className ="soilClass"
-imgSoil.style.width="100%";
-imgSoil.style.height="100%";
-// const soilHtml =document.querySelector(".container");
-imgSoil.title = 'Image title';
-imgSoil.style.gridColumn ="${x} / ${x + 1}";
-// imgSoil.style.gridRow ="${i} / ${i + 1}";
-soilHtml.appendChild(imgSoil);
+let stoneCounter = 0;
+let stonebutton = document.createElement("button");
+soilHtml.appendChild(stonebutton);
+stonebutton.textContent = `grass = ${stoneCounter}`;
+
+for(let i = 1; i < 4; i++) {
+    for(let x = 1; x < 11; x++) {
+        //soil
+        const imgSoil = document.createElement("img");
+        imgSoil.src = "soil1.png";
+        imgSoil.className = "soilClass";
+        imgSoil.style.width = "100%";
+        imgSoil.style.height = "100%";
+        imgSoil.style.gridColumn = `${x} / ${x + 1}`;
+        imgSoil.style.gridRow = `${i + 4} / ${i + 5}`; // Start from row 5
+        soilHtml.appendChild(imgSoil);
+        //stone
+        const imgStone = document.createElement("img");
+        imgStone.src = "ssss.jpg";
+        imgStone.className = "stoneClass";
+        imgStone.style.width = "100%";
+        imgStone.style.height = "100%";
+        imgStone.style.gridColumn = `${x} / ${x + 1}`;
+        imgStone.style.gridRow = `${i + 7} / ${i + 8}`; // Start from row 8
+        soilHtml.appendChild(imgStone);
     }
+}
+let grassCounter = 0;
+let grassbutton = document.createElement("button");
+soilHtml.appendChild(grassbutton);
+grassbutton.textContent = `grass = ${grassCounter}`;
+
+const axe =document.getElementById("axe");
+
+for(let x = 1; x < 11; x++) {
+    //grass
+    const imgGrass = document.createElement("img");
+    imgGrass.src = "grass1.png";
+    // imgGrass.className = "grassClass";
+    imgGrass.style.width = "100%";
+    imgGrass.style.height = "100%";
+    imgGrass.style.gridColumn = `${x} / ${x + 1}`;
+    imgGrass.style.gridRow = `4 / 5`; 
+    soilHtml.appendChild(imgGrass);
+    axe.addEventListener("click", () => {
+        imgGrass.addEventListener("click", () => {
+            imgGrass.remove();
+            grassCounter++;
+            grassbutton.textContent = `grass = ${grassCounter}`;
+        });
+    });
 }
