167c7ec38fcf1a5a86e71b1dc34372a8a0572bf1
nana232002 <nanaomar457@gmail.com>
2024-10-14 08:47:16 +0300

    ok

--- minecraft.js
+++ minecraft.js
@@ -22,13 +22,6 @@ for(let i = 1; i < 4; i++) {
         imgSoil.style.gridColumn = `${x} / ${x + 1}`;
         imgSoil.style.gridRow = `${i + 4} / ${i + 5}`; // Start from row 5
         soilHtml.appendChild(imgSoil);
-        pickaxe.addEventListener("click", () => {
-            imgSoil.addEventListener("click", () => {
-                imgSoil.remove();
-                soilCounter++;
-                soilbutton.textContent = `soil = ${soilCounter}`;
-            });
-        });
         //stone
         const imgStone = document.createElement("img");
         imgStone.src = "ssss.jpg";
@@ -38,77 +31,152 @@ for(let i = 1; i < 4; i++) {
         imgStone.style.gridColumn = `${x} / ${x + 1}`;
         imgStone.style.gridRow = `${i + 7} / ${i + 8}`; // Start from row 8
         soilHtml.appendChild(imgStone);
-        shovel.addEventListener("click", () => {
-            imgStone.addEventListener("click", () => {
-                imgStone.remove();
-                stoneCounter++;
-                stonebutton.textContent = `stone = ${stoneCounter}`;
-            });
-        });
+        
     }
 }
+
 let grassCounter = 0;
 let grassbutton = document.createElement("button");
 soilHtml.appendChild(grassbutton);
 grassbutton.textContent = `grass = ${grassCounter}`;
 
-const axe =document.getElementById("axe");
-document.addEventListener("DOMContentLoaded",()=>{
-    soilHtml.addEventListener("click",(event)=>{
-        console.log(event.target);
+// const axe =document.getElementById("axe");
+// document.addEventListener("DOMContentLoaded",()=>{
+//     soilHtml.addEventListener("click",(event)=>{
+//         console.log(event.target);
         
-        });
-});
+//         });
+// });
 for(let x = 1; x < 11; x++) {
     //grass
     createGrass(x,4);
 }
 
-// grassbutton.addEventListener("click", () => {
-//     const gridContainer = document.querySelector(".container");
-//     gridContainer.addEventListener("click", (event) => {
-//         if (grassCounter > 0) {
-//             const gridRect = gridContainer.getBoundingClientRect();
-//             const gridColumn = Math.floor((event.clientX - gridRect.left) / (gridRect.width / 10)) + 1; // Assuming 10 columns
-//             const gridRow = Math.floor((event.clientY - gridRect.top) / (gridRect.height / 10)) + 1; // Assuming 10 rows
-//             if (!isCellOccupied(gridContainer, gridColumn, gridRow)) {
-//             createGrass(gridColumn,gridRow );
-//             grassCounter--;
-//             grassbutton.textContent = `grass = ${grassCounter}`;
-//             }
-//         }
-        
-//     });
-// });
-
-function isCellOccupied(gridContainer, gridColumn, gridRow) {
-    const children = gridContainer.children;
-    for (let i = 0; i < children.length; i++) {
-        const child = children[i];
-        const childColumnStart = parseInt(window.getComputedStyle(child).gridColumnStart);
-        const childRowStart = parseInt(window.getComputedStyle(child).gridRowStart);
-
-        if (childColumnStart === gridColumn && childRowStart === gridRow) {
-            return true; // The grid cell is already occupied
-        }
-    }
-    return false; // The grid cell is free
-}
 function createGrass (x ,y){
     const axe =document.getElementById("axe");
     const imgGrass = document.createElement("img");
     imgGrass.src = "grass1.png";
-    // imgGrass.className = "grassClass";
+    imgGrass.className = "grassClass";
     imgGrass.style.width = "100%";
     imgGrass.style.height = "100%";
     imgGrass.style.gridColumn = `${x} / ${x + 1}`;
     imgGrass.style.gridRow = `${y} / ${y + 1}`; 
     soilHtml.appendChild(imgGrass);
-    axe.addEventListener("click", () => {
-        imgGrass.addEventListener("click", () => {
-            imgGrass.remove();
-            grassCounter++;
+}
+
+let selectedItem;
+let selectedTool;
+
+pickaxe.addEventListener("click", () => {
+    selectedTool="pickaxe";
+    selectedItem =undefined;
+});
+shovel.addEventListener("click", () => {
+    selectedTool="shovel";
+    selectedItem =undefined;
+});
+axe.addEventListener("click", () => {
+    selectedTool="axe";
+    selectedItem =undefined;
+    
+});
+
+soilbutton.addEventListener("click", () => {
+    selectedItem="soilButton";
+    selectedTool =undefined;
+});
+grassbutton.addEventListener("click", () => {
+    selectedItem="grassButton";
+    selectedTool =undefined;
+});
+stonebutton.addEventListener("click", () => {
+    selectedItem="stoneButton";
+    selectedTool =undefined;
+});
+
+
+
+
+soilHtml.addEventListener("click", (event) => {
+    handleClick(event.target);
+    console.log(grassCounter);
+    console.log(soilCounter);
+    console.log(stoneCounter);
+    
+    
+    
+    console.log(selectedTool);
+    console.log(event.target);
+});
+
+
+function handleClick(clickedTile) {
+if (selectedTool) {
+    if (selectedTool === "pickaxe") {
+        if(clickedTile.className ==="soilClass"){
+            clickedTile.remove();
+            soilCounter ++;
+            soilbutton.textContent = `soil = ${soilCounter}`;
+            
+        }
+    }
+    else if(selectedTool === "axe"){
+        
+        if(clickedTile.className ==="grassClass"){
+            clickedTile.remove();
+            grassCounter ++;
             grassbutton.textContent = `grass = ${grassCounter}`;
-        });
-    });
+            
+        }
+
+    }
+    else if(selectedTool === "shovel"){
+        if(clickedTile.className ==="stoneClass"){
+            clickedTile.remove();
+            stoneCounter ++;
+            stonebutton.textContent = `stone = ${stoneCounter}`;
+        }
+
+    }
+}
+else if(selectedItem){
+    console.log(selectedItem);
+    
+    if (selectedItem === "soilButton") {
+        if(!(clickedTile.className ==="soilClass" || clickedTile.className ==="grassClass" || clickedTile.className ==="stoneClass")){
+            // clickedTile.remove();
+            // soilCounter ++;
+            // soilbutton.textContent = `soil = ${soilCounter}`;
+            // console.log("ok");
+            
+            
+        }
+    }
+    if (selectedItem === "grassButton") {
+        if(!(clickedTile.className ==="soilClass" || clickedTile.className ==="grassClass" || clickedTile.className ==="stoneClass")){
+            // createGrass(clickedTile.gridColumn ,clickedTile.gridRow);
+            const imgGrass = document.createElement("img");
+            imgGrass.src = "grass1.png";
+            imgGrass.className = "grassClass";
+            imgGrass.style.width = "100%";
+            imgGrass.style.height = "100%";
+            clickedTile.appendChild(imgGrass);
+            // const x=clickedTile.gridColumn;
+            // const y=clickedTile.gridRow;
+            // console.log(x);
+            // console.log(y);
+            
+            
+            // imgGrass.style.gridColumn = `${x} / ${x + 1}`;
+            // imgGrass.style.gridRow = `${y} / ${y + 1}`; 
+            soilHtml.appendChild(imgGrass);
+            console.log("ok");
+            
+            
+        }
+    }
+}
+    
+
+
 }
